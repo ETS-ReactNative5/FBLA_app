7e862b28ee61f71afb53c612e8698122e0a8131f
Ethan <ethan.s127@gmail.com>
2019-11-10 14:23:23 -0700

    License

--- source_code_app/screens/LicenseInfo.js
+++ source_code_app/screens/LicenseInfo.js
@@ -11,10 +11,12 @@ export default class LicenseScreen extends React.Component {
   
     render() {
       return (
-        <ScrollView style={styles.container,{textAlign: 'left'}}>
+        <View style={styles.container,{textAlign: 'left'}}>
             <Text style={styles.licenseText}>
-                MIT License
-                Copyright (c) 2019 Ethan127, WackyWolff
+                <Text style={{textAlign: 'center'}}>
+                    MIT License{"\n"}
+                    Copyright (©) 2019 Ethan127, WackyWolff
+                </Text>
                 {"\n\n"}
                 Permission is hereby granted, free of charge, to any person obtaining a copy
                 of this software and associated documentation files (the "Software"), to deal
@@ -22,10 +24,10 @@ export default class LicenseScreen extends React.Component {
                 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
                 copies of the Software, and to permit persons to whom the Software is
                 furnished to do so, subject to the following conditions:
-
+                {"\n\n"}
                 The above copyright notice and this permission notice shall be included in all
                 copies or substantial portions of the Software.
-
+                {"\n\n"}
                 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
                 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
                 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -34,7 +36,7 @@ export default class LicenseScreen extends React.Component {
                 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
                 SOFTWARE.
             </Text>
-        </ScrollView>
+        </View>
       );
     }
   }
