5c0f8ba7bc165f5cab60fbf34a1a8bc1351e0c28
Ethan <ethan.s127@gmail.com>
2019-11-11 12:18:02 -0700

    Made stuff better

--- source_code_app/screens/LoginScreen.js
+++ source_code_app/screens/LoginScreen.js
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from "react";
 import {
     ActivityIndicator,
+    Text,
+    Keyboard,
     AsyncStorage,
     Button,
     StatusBar,
     StyleSheet,
+    TextInput,
+    TouchableWithoutFeedback,
     View,
+    Alert, 
+    KeyboardAvoidingView,
   } from 'react-native';
-const styles = StyleSheet.create(require('../stylesheet'));
+const styles = StyleSheet.create(require('../loginstyle.js'));
 import { AuthSession } from 'expo';
 
 
-export default class LoginScreen extends React.Component {
+/*export default class LoginScreen extends React.Component {
     static navigationOptions = {
         title: 'Please sign in',
     };
@@ -19,7 +25,7 @@ export default class LoginScreen extends React.Component {
     render() {
         return (
           <View style={styles.container}>
-            <Button title="Sign in!" onPress={this._signInAsync} />
+            <Button title="( ͡❛ ͜ʖ ͡❛)Sign in!( ͡❛ ⏏ ͡❛)" onPress={this._signInAsync} />
           </View>
         );
     }
@@ -28,8 +34,51 @@ export default class LoginScreen extends React.Component {
         await AsyncStorage.setItem('userToken', 'abc');
         this.props.navigation.navigate('App');
     };
-}
+}*/
 
-LoginScreen.navigationOptions = {
-    title: 'Please sign in',
-};
+const appId = "1047121222092614"
+
+export default class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Please Sign In',
+  };
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('App');
+  };
+
+  render() {
+    return (
+        <View style={styles.loginScreenContainer}>
+          <View style={styles.loginFormView}>
+          <Text style={styles.logoText}>FBLA App</Text>
+            <TextInput placeholder="Username" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} />
+            <TextInput placeholder="Password" placeholderColor="#c4c3cb" style={styles.loginFormTextInput} secureTextEntry={true}/>
+            <Button color="#3897f1" title="Login" onPress={this._signInAsync} />
+            <Button
+              buttonStyle={styles.fbLoginButton}
+              onPress={() => this.onFbLoginPress()}
+              title="Login with Facebook"
+              color="#3897f1"
+            />
+          </View>
+        </View>
+
+    );
+  }
+
+  async onFbLoginPress() {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(appId, {
+      permissions: ['public_profile', 'email'],
+    });
+    if (type === 'success') {
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`);
+      Alert.alert(
+        'Logged in!',
+        `Hi ${(await response.json()).name}!`,
+      );
+    }
+  }
+}
